b117d21fade4ca05e5f32f9b3f1fb98f7930b3fa
IsayIsee <solin.zhan@gmail.com>
2026-06-05 14:52:31 +0800

    * 清除缓存改为长按

--- app/src/mobile/java/com/fongmi/android/tv/ui/fragment/SettingFragment.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/fragment/SettingFragment.java
@@ -127,6 +127,7 @@ public class SettingFragment extends BaseFragment implements ConfigListener, Sit
         mBinding.size.setOnClickListener(this::setSize);
         mBinding.uiScale.setOnClickListener(this::setUiScale);
         mBinding.cache.setOnClickListener(this::onCache);
+        mBinding.cache.setOnLongClickListener(this::onLongCache);
         mBinding.backup.setOnClickListener(this::onBackup);
         mBinding.enhance.setOnClickListener(this::onEnhance);
         mBinding.player.setOnClickListener(this::onPlayer);
@@ -326,12 +327,17 @@ public class SettingFragment extends BaseFragment implements ConfigListener, Sit
     }
 
     private void onCache(View view) {
+        Notify.show(R.string.setting_cache_tip);
+    }
+
+    private boolean onLongCache(View view) {
         FileUtil.clearCache(new Callback() {
             @Override
             public void success() {
                 setCacheText();
             }
         });
+        return true;
     }
 
     private void onBackup(View view) {
